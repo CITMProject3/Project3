52a29feb2708e9a506e8202b55577c29c3426c2c
Ruben Sardon <ec4who@hotmail.com>
2017-02-23 05:25:32 +0100

    Basic Car creation and controls
    Separate Module instead of GO (temporarily)
    Missing game_states control & doesn't render correctly.

--- ModuleCar.h
+++ ModuleCar.h
@@ -5,6 +5,11 @@
 #include "Globals.h"
 #include <vector>
 
+#define MAX_ACCELERATION 1000.0f
+#define TURN_DEGREES 15.0f * DEGTORAD
+#define BRAKE_POWER 500.0f
+
+struct PhysVehicle3D;
 
 using namespace std;
 
@@ -21,6 +26,24 @@ public:
 	update_status PostUpdate();
 	bool CleanUp();
 
+	/*/ roads
+	void AddStraightRoad(int large, const vec& pos, int orientation);
+	void AddCurve(const int large, const vec& pos, int orientation);
+	void AddCurveX(const int large, const vec& pos, int orientation);
+	void AddCorner(const int large, const vec& pos, int orientation);
+	void AddCornerX(const int large, const vec& pos, int orientation);
+	void RenderRoads();*/
+
+	list<Cube_P*> roads;
+
+	void AddDummyCar();
+
+private:
+	// Vehicle control
+	PhysVehicle3D* vehicle = nullptr;
+	float turn;
+	float acceleration;
+	float brake;
 	
 };
 
