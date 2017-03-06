248e52fc7e47acdb9482ea0632abbb6c702f1856
Vulpem <davidhl2@TR12.CAT>
2017-03-06 12:55:11 +0100

    Fixed texture-less GameObjects going black

--- ModuleRenderer3D.cpp
+++ ModuleRenderer3D.cpp
@@ -303,11 +303,6 @@ void ModuleRenderer3D::Draw(GameObject* obj, const LightInfo& light, ComponentCa
 	GLint view_location = glGetUniformLocation(shader_id, "view");
 	glUniformMatrix4fv(view_location, 1, GL_FALSE, *cam->GetViewMatrix().v);	
 
-	if (material->texture_ids.empty() == true)
-	{
-		glBindTexture(GL_TEXTURE_2D, 0);
-	}
-
 	int count = 0;
 	//Good code for textures. The code above must be removed.
 	for (map<string, uint>::iterator tex = material->texture_ids.begin(); tex != material->texture_ids.end(); ++tex)
@@ -348,6 +343,14 @@ void ModuleRenderer3D::Draw(GameObject* obj, const LightInfo& light, ComponentCa
 		}
 	}
 	
+	if (material->texture_ids.empty() == true)
+	{
+		GLint has_tex_location = glGetUniformLocation(shader_id, "_HasTexture");
+		glUniform1i(has_tex_location, 0);
+		GLint has_normal_location = glGetUniformLocation(shader_id, "_HasNormalMap");
+		glUniform1i(has_normal_location, 0);
+		glBindTexture(GL_TEXTURE_2D, 0);
+	}
 
 	//Textures
 	/*if (material->GetDiffuseId() != 0)
