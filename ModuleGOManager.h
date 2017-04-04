a06c8edbda76ca19a19a05aaba82c774ad8e26b7
markitus18 <marc18.garrigo@gmail.com>
2017-04-05 01:12:06 +0200

    Scene ready for vehicle and characters replacement

--- ModuleGOManager.h
+++ ModuleGOManager.h
@@ -126,10 +126,10 @@ public:
 	uint team2_car = 1;
 
 	uint team1_p1_c = 0;
-	uint team1_p2_c = 0;
+	uint team1_p2_c = 2;
 
-	uint team2_p1_c = 0;
-	uint team2_p2_c = 0;
+	uint team2_p1_c = 1;
+	uint team2_p2_c = 3;
 
 	PLAYER team1_front;
 	PLAYER team1_back;
