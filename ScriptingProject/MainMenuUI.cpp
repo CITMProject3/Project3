8828196c0f195262afcf2a8c1ed97ef71980a7cf
auusi9 <ausiasdr@TR12.CAT>
2017-04-04 17:56:22 +0200

    Start menu done

--- ScriptingProject/MainMenuUI.cpp
+++ ScriptingProject/MainMenuUI.cpp
@@ -22,18 +22,21 @@ namespace Main_Menu_UI
 	GameObject* select_menu = nullptr;
 	GameObject* select_level = nullptr;
 	ComponentCanvas* canvas = nullptr;
+	int current_scene = 0;
+	int current_canvas_scene = 0;
 
 	void Main_Menu_UI_GetPublics(map<const char*, string>* public_chars, map<const char*, int>* public_ints, map<const char*, float>* public_float, map<const char*, bool>* public_bools, map<const char*, GameObject*>* public_gos)
 	{
 		public_gos->insert(std::pair<const char*, GameObject*>("Start Menu", start_menu));
 		public_gos->insert(std::pair<const char*, GameObject*>("Select character Menu", select_menu));
 		public_gos->insert(std::pair<const char*, GameObject*>("Select level Menu", select_level));
+		public_ints->insert(std::pair<const char*, int>("current_menu", current_scene));
 	}
 
 	void Main_Menu_UI_UpdatePublics(GameObject* game_object)
 	{
 		ComponentScript* test_script = (ComponentScript*)game_object->GetComponent(ComponentType::C_SCRIPT);
-
+		current_scene = test_script->public_ints.at("current_menu");
 		start_menu = test_script->public_gos.at("Start Menu");
 		select_menu = test_script->public_gos.at("Select character Menu");
 		select_level = test_script->public_gos.at("Select level Menu");
@@ -43,7 +46,7 @@ namespace Main_Menu_UI
 	void Main_Menu_UI_ActualizePublics(GameObject* game_object)
 	{
 		ComponentScript* test_script = (ComponentScript*)game_object->GetComponent(ComponentType::C_SCRIPT);
-
+		test_script->public_ints.at("current_menu") = current_scene;
 		test_script->public_gos.at("Start Menu") = start_menu;
 		test_script->public_gos.at("Select character Menu") = select_menu;
 		test_script->public_gos.at("Select level Menu") = select_level;
@@ -55,12 +58,37 @@ namespace Main_Menu_UI
 	void Main_Menu_UI_Start(GameObject* game_object)
 	{
 		if (start_menu != nullptr)
+		{
 			canvas->SetGoFocus(start_menu);
+			select_level->SetActive(false);
+			select_menu->SetActive(false);
+			
+		}
+			
 		//canvas->go_focus = start_menu;
 	}
 
 	void Main_Menu_UI_Update(GameObject* game_object)
 	{
-
+		if (current_canvas_scene != current_scene)
+		{
+			switch (current_scene)
+			{
+			case 1:
+				start_menu->SetActive(false);
+				select_level->SetActive(false);
+				select_menu->SetActive(true);
+				canvas->SetGoFocus(select_menu);
+				break;
+			case 2:
+				start_menu->SetActive(false);
+				select_level->SetActive(true);
+				select_menu->SetActive(false);
+				canvas->SetGoFocus(select_level);
+				break;
+			}
+			current_canvas_scene = current_scene;
+		}
+		
 	}
 }
--- ScriptingProject/Start_Menu_UI.cpp
+++ ScriptingProject/Start_Menu_UI.cpp
@@ -15,6 +15,8 @@
 #include "../SDL/include/SDL_scancode.h"
 #include "../Globals.h"
 #include "../ComponentUiButton.h"
+#include "../ModuleGOManager.h"
+#include "../ComponentCanvas.h"
 
 namespace Start_Menu_UI
 {
@@ -37,15 +39,15 @@ namespace Start_Menu_UI
 
 	void Start_Menu_UI_GetPublics(map<const char*, string>* public_chars, map<const char*, int>* public_ints, map<const char*, float>* public_float, map<const char*, bool>* public_bools, map<const char*, GameObject*>* public_gos)
 	{
-		public_gos->insert(std::pair<const char*, GameObject*>("RB Button", rb_button));
-		public_gos->insert(std::pair<const char*, GameObject*>("RT Button", rt_button));
 		public_gos->insert(std::pair<const char*, GameObject*>("LB Button", lb_button));
 		public_gos->insert(std::pair<const char*, GameObject*>("LT Button", lt_button));
+		public_gos->insert(std::pair<const char*, GameObject*>("RB Button", rb_button));
+		public_gos->insert(std::pair<const char*, GameObject*>("RT Button", rt_button));
 
-		player_order[0] = -1;
-		player_order[1] = -1;
-		player_order[2] = -1;
-		player_order[3] = -1;
+		public_ints->insert(std::pair<const char*, int>("Player1", player_order[0]));
+		public_ints->insert(std::pair<const char*, int>("Player2", player_order[1]));
+		public_ints->insert(std::pair<const char*, int>("Player3", player_order[2]));
+		public_ints->insert(std::pair<const char*, int>("Player4", player_order[3]));
 	}
 
 	void Start_Menu_UI_UpdatePublics(GameObject* game_object)
@@ -57,10 +59,17 @@ namespace Start_Menu_UI
 		lb_button = test_script->public_gos.at("LB Button");
 		lt_button = test_script->public_gos.at("LT Button");
 
+		player_order[0] = test_script->public_ints.at("Player1");
+		player_order[1] = test_script->public_ints.at("Player2");
+		player_order[2] = test_script->public_ints.at("Player3");
+		player_order[3] = test_script->public_ints.at("Player4");
+
 		c_rb = (ComponentUiButton*)rb_button->GetComponent(C_UI_BUTTON);
 		c_rt = (ComponentUiButton*)rt_button->GetComponent(C_UI_BUTTON);
 		c_lb = (ComponentUiButton*)lb_button->GetComponent(C_UI_BUTTON);
 		c_lt = (ComponentUiButton*)lt_button->GetComponent(C_UI_BUTTON);
+
+
 	}
 
 	void Start_Menu_UI_ActualizePublics(GameObject* game_object)
@@ -72,6 +81,11 @@ namespace Start_Menu_UI
 		test_script->public_gos.at("LB Button") = lb_button;
 		test_script->public_gos.at("LT Button") = lt_button;
 
+		test_script->public_ints.at("Player1") = player_order[0];
+		test_script->public_ints.at("Player2") = player_order[1];
+		test_script->public_ints.at("Player3") = player_order[2];
+		test_script->public_ints.at("Player4") = player_order[3];
+
 		c_rb = (ComponentUiButton*)rb_button->GetComponent(C_UI_BUTTON);
 		c_rt = (ComponentUiButton*)rt_button->GetComponent(C_UI_BUTTON);
 		c_lb = (ComponentUiButton*)lb_button->GetComponent(C_UI_BUTTON);
@@ -82,55 +96,22 @@ namespace Start_Menu_UI
 
 	void Start_Menu_UI_Start(GameObject* game_object)
 	{
+		player_order[0] = -1;
+		player_order[1] = -1;
+		player_order[2] = -1;
+		player_order[3] = -1;
+
+		rb_pressed = false;
+		rt_pressed = false;
+		lb_pressed = false;
+		lt_pressed = false;
 	}
 
 	void Start_Menu_UI_Update(GameObject* game_object)
 	{
 		for (int i = 0; i < 4; i++)
 		{
-			if (rb_pressed == false)
-			{	
-				if (App->input->GetJoystickButton(i, JOY_BUTTON::RB) == KEY_DOWN || App->input->GetKey(SDL_SCANCODE_R) == KEY_DOWN)
-				{
-					bool selectable = true;
-					for (int j = 0; j < 4; j++)
-					{
-						if (player_order[j] == i)
-						{
-							selectable = false;
-						}
-					}
-					if (selectable)
-					{
-						player_order[0] = i;
-						c_rb->OnPressId(i);
-						rb_pressed = true;
-					}
-					
-				}
-			}
 			
-			
-			if (rt_pressed == false)
-			{
-				if (App->input->GetJoystickAxis(i, JOY_AXIS::RIGHT_TRIGGER) > 0.2f || App->input->GetKey(SDL_SCANCODE_Y) == KEY_DOWN)
-				{
-					bool selectable = true;
-					for (int j = 0; j < 4; j++)
-					{
-						if (player_order[j] == i)
-						{
-							selectable = false;
-						}
-					}
-					if (selectable)
-					{
-						player_order[1] = i;
-						c_rt->OnPressId(i);
-						rt_pressed = true;
-					}
-				}
-			}
 
 			if (lb_pressed == false)
 			{
@@ -146,7 +127,7 @@ namespace Start_Menu_UI
 					}
 					if (selectable)
 					{
-						player_order[2] = i;
+						player_order[0] = i;
 						c_lb->OnPressId(i);
 						lb_pressed = true;
 					}
@@ -167,9 +148,53 @@ namespace Start_Menu_UI
 					}
 					if (selectable)
 					{
-						player_order[3] = i;
+						player_order[1] = i;
 						c_lt->OnPressId(i);
 						lt_pressed = true;
+					}
+				}
+			}
+
+			if (rb_pressed == false)
+			{
+				if (App->input->GetJoystickButton(i, JOY_BUTTON::RB) == KEY_DOWN || App->input->GetKey(SDL_SCANCODE_R) == KEY_DOWN)
+				{
+					bool selectable = true;
+					for (int j = 0; j < 4; j++)
+					{
+						if (player_order[j] == i)
+						{
+							selectable = false;
+						}
+					}
+					if (selectable)
+					{
+						player_order[2] = i;
+						c_rb->OnPressId(i);
+						rb_pressed = true;
+					}
+
+				}
+			}
+
+
+			if (rt_pressed == false)
+			{
+				if (App->input->GetJoystickAxis(i, JOY_AXIS::RIGHT_TRIGGER) > 0.2f || App->input->GetKey(SDL_SCANCODE_Y) == KEY_DOWN)
+				{
+					bool selectable = true;
+					for (int j = 0; j < 4; j++)
+					{
+						if (player_order[j] == i)
+						{
+							selectable = false;
+						}
+					}
+					if (selectable)
+					{
+						player_order[3] = i;
+						c_rt->OnPressId(i);
+						rt_pressed = true;
 					}
 				}
 			}
@@ -184,32 +209,32 @@ namespace Start_Menu_UI
 						switch (j)
 						{
 						case 0:
-							if (rb_pressed)
-							{
-								rb_pressed = false;
-								c_rb->OnPressId(i);
-							}
-							
-							break;
-						case 1:
-							if (rt_pressed)
-							{
-								rt_pressed = false;
-								c_rt->OnPressId(i);
-							}
-							break;
-						case 2:
 							if (lb_pressed)
 							{
 								lb_pressed = false;
 								c_lb->OnPressId(i);
 							}
 							break;
-						case 3:
+						case 1:
 							if (lt_pressed)
 							{
 								lt_pressed = false;
 								c_lt->OnPressId(i);
+							}
+							break;
+						case 2:
+							if (rb_pressed)
+							{
+								rb_pressed = false;
+								c_rb->OnPressId(i);
+							}
+
+							break;
+						case 3:
+							if (rt_pressed)
+							{
+								rt_pressed = false;
+								c_rt->OnPressId(i);
 							}
 							break;
 						}
@@ -217,6 +242,23 @@ namespace Start_Menu_UI
 					}
 				}
 			}
+
+			int total = 0;
+			for (int j = 0; j < 4; j++)
+			{
+				total += player_order[j];
+
+				if (total == 6)
+				{
+					if (App->input->GetJoystickButton(i, JOY_BUTTON::START) == KEY_DOWN || App->input->GetKey(SDL_SCANCODE_SPACE) == KEY_DOWN)
+					{
+						ComponentScript* main_canvas_script = (ComponentScript*)App->go_manager->current_scene_canvas->GetGameObject()->GetComponent(C_SCRIPT);
+
+						main_canvas_script->public_ints.at("current_menu") = 1;
+					}
+				}
+			}
+
 		}
 	}
 }
