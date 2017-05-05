e138e20a5e4bffdfcbb4ba4dfe2c4eb59df0d1df
auusi9 <ausi_dalmau@hotmail.com>
2017-05-05 03:29:26 +0200

    Final build changes

--- ScriptingProject/MapSelectUI.cpp
+++ ScriptingProject/MapSelectUI.cpp
@@ -39,8 +39,8 @@ namespace MapSelectUI
 	ComponentUiButton* c_right_arrow = nullptr;
 	ComponentUiButton* c_left_arrow = nullptr;
 
-	string path_map1 = "/Assets/Scene_Map_2/Scene_Map_2.ezx";
-	string path_map2 = "/Assets/Scene_Map_1/Scene_Map_1.ezx";
+	string path_map1 = "/Assets/Scene_Map_1/Scene_Map_1.ezx";
+	string path_map2 = "/Assets/Scene_Map_2/Scene_Map_2.ezx";
 	string path_map3 = "/Assets/Scene_Map_2/Scene_Map_2.ezx";
 
 	bool players_ready[4] = { false, false, false, false };
